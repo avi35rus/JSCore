285bafce6b2bcaa4851530d68e307469891861b0
Aleksandrov Vladimir <avi35rus@gmail.com>
2023-05-20 21:00:03 +0300

    1.1.14

---
+++ 1/1.1.14.js
@@ -0,0 +1,7 @@
+const defaultTo = (value, defaultValue) => {
+	if (!value) {
+		return defaultValue
+	} else {
+		return value
+	}
+};
---
+++ 1/1.1.js
@@ -0,0 +1,23 @@
+// let value;
+
+// value = String([1, 2, 3])
+// value = String({ name: 'Vladimir' })
+
+// value = true + 10
+// value = []
+
+// console.log(value)
+// console.log(typeof value)
+
+
+
+
+// const defaultTo = (value, defaultValue) => {
+// 	if (!value) {
+// 		return defaultValue
+// 	} else {
+// 		return value
+// 	}
+// };
+
+
