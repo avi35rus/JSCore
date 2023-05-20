a6c324d0479db0413a323a5514a75706d6fd7a49
Aleksandrov Vladimir <avi35rus@gmail.com>
2023-05-20 20:13:04 +0300

    1.1.4

---
+++ 1/1.1.4.js
@@ -0,0 +1,11 @@
+1.1.4
+function reference() {
+	return missingVar
+}
+
+function type() {
+  const ADMIN_PASSWORD = '123'
+  const admin_password = ADMIN_PASSWORD(12345)
+
+  return admin_password
+}
